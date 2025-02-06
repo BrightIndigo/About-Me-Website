9159e75a250e6d96759f52f672667211d4f32004
Krzysztof Wilgucki <128691376+BrightIndigo@users.noreply.github.com>
2025-02-06 13:19:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html', name='Twoje Imię', bio='Krótki opis o mnie.', image_url='static/profile.jpg', social_links={'GitHub': 'https://github.com/BrightIndigo', 'LinkedIn': 'https://www.linkedin.com/in/krzysztof-wilgucki-aa7b0a2a4/'})
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route('/about')
+def about_me():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
